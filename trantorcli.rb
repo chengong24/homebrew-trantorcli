7926d3c2465e54fc440a0eb8543194f1e0acb8bf
chengong <cg268505@alibaba-inc.com>
2021-08-31 12:27:39 +0800

    init file

---
+++ trantorcli.rb
@@ -0,0 +1,35 @@
+class TrantorCli < Formula
+  desc "Terminus Trantor CLI"
+  homepage "https://www.terminus.io/"
+  url "http://mxsl.oss-cn-hangzhou.aliyuncs.com/dist/trantor/trantor.0.18.0.tar.gz"
+  version "0.18.0"
+  sha256 "c5f267f95ce1a6ddd1b35ab174a0beab17ddceb6950daa73d548a10178edbb3f"
+
+#   depends_on "docker"
+
+  def buildExe()
+    <<~EOS
+      #!/bin/bash
+      if [ -z "$JAVA_HOME" ] ; then
+        JAVACMD=`which java`
+      else
+        JAVACMD="$JAVA_HOME/bin/java"
+      fi
+      export TRANTOR_HOME="#{prefix}"
+      export TRANTOR_CLI_VERSION="0.18.0"
+      exec "$JAVACMD" -jar "#{libexec}/trantor-cli.jar" "$@"
+    EOS
+  end
+
+  def install
+    # Remove windows files
+    lib.install Dir["lib/*"]
+    libexec.install Dir["libexec/*"]
+    (bin/"trantorcli").write buildExe()
+  end
+
+  test do
+    system "#{bin}/trantorcli", "version"
+  end
+
+end
