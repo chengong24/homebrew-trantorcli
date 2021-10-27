b7fde8268ef935f9f0f13614d762a21f1fe5bd01
chengong24 <841966612@qq.com>
2021-10-27 14:13:59 +0800

    update trantor cli version to [0.0.1-SNAPSHOT], trantor commit[daa8c7bdf0960178ad6acd6e4851e768abfc6679]

--- trantorcli.rb
+++ trantorcli.rb
@@ -1,11 +1,9 @@
-class Trantorcli< Formula
+class Trantor < Formula
   desc "Terminus Trantor CLI"
   homepage "https://www.terminus.io/"
   url "https://terminus-trantor.oss-cn-hangzhou.aliyuncs.com/tools/cli/trantor-cli.latest.tar.gz"
-  version "2.0.RELEASE"
-  sha256 "f8ca8f18c0f90de072524d20e094915e1381e2b8b1d60104478591ddf11d4319"
-
-#   depends_on "docker"
+  version "0.0.1-SNAPSHOT"
+  sha256 "c8a1f8f8178bf30697ab88dd0177f716cc878f2a7302851ca6fef9f2a5484048"
 
   def install
     # Remove windows files
@@ -15,7 +13,6 @@ class Trantorcli< Formula
    fish_completion.install "completions/fish/trantor-completion.fish"
   end
 
-  
   test do
     system "#{bin}/trantor", "version"
   end
